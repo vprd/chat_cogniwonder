43c01a97d910cd0e681ed30e49a68949eeb71e47
Raj <raj.fps2000@gmail.com>
2020-07-03 17:24:22 +0530

    Update api-endpoint.js

--- client/src/api-endpoint.js
+++ client/src/api-endpoint.js
@@ -1,4 +1,11 @@
+require('dotenv').config();
 export default function endpoint() {
-    window.API_ENDPOINT = 'http://localhost:8000/';
-    return window.API_ENDPOINT;
+
+    if (process.env.ENVIRONMENT === 'local') {
+        window.API_ENDPOINT = 'http://localhost:8000/';
+        return window.API_ENDPOINT;
+    }else{
+        window.API_ENDPOINT = 'http://chat.cogniwonder.com/';
+        return window.API_ENDPOINT;
+    }
 }
