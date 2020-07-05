53d4ee4029ffc859f979874092f6e1885cb90b5b
Raj <raj.fps2000@gmail.com>
2020-07-05 17:31:13 +0530

    deploying-server-and-client

---
+++ client/build/precache-manifest.66e49a07b0bac930da440ec28af1983b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b906aa73eb2a8cb54afeb20916971e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ff608ebf89a75df092a",
+    "url": "/static/css/main.4cbe9d98.chunk.css"
+  },
+  {
+    "revision": "117be4bedee143ee2b45",
+    "url": "/static/js/2.5493a983.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.5493a983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ff608ebf89a75df092a",
+    "url": "/static/js/main.0d1ffddd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "2b68e57dcaa02dd685f70b156aa76065",
+    "url": "/static/media/logo.2b68e57d.png"
+  }
+]);
