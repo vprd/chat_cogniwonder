f26ec1ffb1d7bb2ce2f3b01b2b08e3205f4d3805
Raj Sharma <xrehpicx@gmail.com>
2020-07-15 22:49:15 +0530

    fix not found

---
+++ client/build/precache-manifest.1bb5aa9ba1a4a835034ec75823219195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a37fceda7fb1703462ad5e10a98f43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8bd95b76fe9edbc2855",
+    "url": "/static/css/main.24867bc4.chunk.css"
+  },
+  {
+    "revision": "613e77c5f0680a141583",
+    "url": "/static/js/2.ea17331a.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.ea17331a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8bd95b76fe9edbc2855",
+    "url": "/static/js/main.abfb091f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "2b68e57dcaa02dd685f70b156aa76065",
+    "url": "/static/media/logo.2b68e57d.png"
+  }
+]);
